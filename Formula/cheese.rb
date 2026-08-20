ebcc0a297200522571cf90e1d9ff14f5dbe2cb5e
Robin <robindev2019@outlook.com>
2026-08-20 15:12:44 +0200

    Release dotfiles/r558.7ae2c0b cipher/r26.48db851 cheese/r1.1eaec60 sandwich/r1.6b73df9

--- Formula/cheese.rb
+++ Formula/cheese.rb
@@ -2,9 +2,9 @@ class Cheese < Formula
   desc "Kaas. Now available online"
   homepage "https://git.dupunkto.org/~dupunkto/cheese"
 
-  url "https://repo.dupunkto.org/cheese-r1.7910419.tar.gz"
-  version "r1.7910419"
-  sha256 "dd88d5bff9ab6ad75a803c5fef1d810c2846962a9084bc394b9e278e5578124c"
+  url "https://repo.dupunkto.org/cheese-r1.1eaec60.tar.gz"
+  version "r1.1eaec60"
+  sha256 "c397bf42d1f6d45e68179a90934095c6d409779efe82102737ad4afbf13f5d15"
 
   license "Unlicense"
 
