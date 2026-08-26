d83c848c038737c1c891c9e6ae3d790854d82612
Robin <robindev2019@outlook.com>
2026-08-26 22:49:45 +0200

    Release dotfiles/r571.b05f573 cipher/r26.48db851 cheese/r2.2f6c685 sandwich/r2.5818b6a

--- Formula/keychain.rb
+++ Formula/keychain.rb
@@ -2,9 +2,9 @@ class Keychain < Formula
   desc "GPG passphrase manager"
   homepage "https://geheimesite.nl"
 
-  url "https://repo.dupunkto.org/dotfiles-r569.aa0d046.tar.gz"
-  sha256 "c7eeeab1f16062ed7cd37242c8788978ecad45802831ff599c09cfb39915f8e4"
-  version "r569.aa0d046"
+  url "https://repo.dupunkto.org/dotfiles-r571.b05f573.tar.gz"
+  sha256 "410e2066967e301451c90971e7d10c1a8fd3bd2bd1d82740bf4fdd44d4ceb750"
+  version "r571.b05f573"
 
   head "https://git.dupunkto.org/meta/dotfiles.git", branch: "master"
 
